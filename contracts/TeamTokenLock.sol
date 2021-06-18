1dc711c70d4f3793d3e13c5337904afedad16687
Jade-alrs <85907526+Jade-alrs@users.noreply.github.com>
2021-06-18 17:48:34 +1000

    Update TeamTokenLock.sol

--- contracts/TeamTokenLock.sol
+++ contracts/TeamTokenLock.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./AllerSafe.sol";
+import "./AllerSafeToken.sol";
 
 contract TeamTokenLock {
     using SafeERC20 for AllerSafe;
